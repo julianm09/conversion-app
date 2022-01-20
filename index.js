2f45c767d99c0d82f280613ceb7ce8e8054b777d
chuncaso90 <72043079+chuncaso90@users.noreply.github.com>
2022-01-20 15:40:38 -0800

    what

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ console.log(add(3, 5));
 console.log(subtract(3, 5));
 
 const getRates = async () => {
+  
   const res = await axios.get(
-    "https://v6.exchangerate-api.com/v6/03c5b60a9aa12a5a3ed27421/latest/USD"
+    "https://v6.exchangerate-api.com/v6/03c5b60a9aa12a5a3ed27421/latest/"
   );
   console.log(res.data);
 };
