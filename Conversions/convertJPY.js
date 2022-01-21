6e9196914bd7753ddb0effdeff29ce9c119a7645
Julian Mayes <juliantmayes@gmail.com>
2022-01-20 16:06:15 -0800

    japanese yen

--- Conversions/convertJPY.js
+++ Conversions/convertJPY.js
@@ -1,17 +1,16 @@
+const axios = require("axios");
+
 const getRates = async () => {
   const res = await axios.get(
     "https://v6.exchangerate-api.com/v6/03c5b60a9aa12a5a3ed27421/latest/JPY"
   );
-  console.log(res.data);
   return res.data;
 };
 
 const convertJPY = async (x, y) => {
   const rates = await getRates();
-  return x * rates[y];
+  const conversion = (await rates.conversion_rates[y]) * x;
+  return conversion;
 };
 
-convertJPY(100, "CAD");
-
-
-module.exports = convertJPY
+module.exports = convertJPY;
