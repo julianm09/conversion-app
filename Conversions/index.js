1513fcc3a062501f82d0920cce61f4b3b1dce130
Julian Mayes <juliantmayes@gmail.com>
2022-01-23 12:17:21 -0800

    added comments

--- Conversions/index.js
+++ Conversions/index.js
@@ -1,6 +1,8 @@
+//import your js file
 const convertJPY = require('./convertJPY');
 const convertEUR = require('./convertEUR');
 
+//export module
 module.exports = {
     convertJPY,
     convertEUR,
