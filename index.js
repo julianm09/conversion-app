bdeff86fdd220dd1ac70007eb61f4d8f518dfb4c
Julian Mayes <61668442+julianmayes@users.noreply.github.com>
2022-01-27 15:16:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ app.use(express.json());
 app.use(conversionRouter);
 app.listen(3000, () =>
   console.log(
-    `server running on 3000"
-post
-    {
-      "fromCurrency": "usd",
-      "toCurrency": "cad",
-      "amount": "10"
-    }`
+    `server running on 3000`
   )
 );
