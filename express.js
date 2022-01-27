e1bce393dcae0e534aa1b8bec5386ab400fb5ddc
chuncaso90 <72043079+chuncaso90@users.noreply.github.com>
2022-01-27 15:59:45 -0800

    express and todo js

---
+++ express.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const todoRouter = require('./Routes/todo')
+
+app.use(todoRouter)
+app.listen(3000, ()=> console.log('server running on 3000'))
